dd96db41615d5bffcb4c254c3b1afe9eb87e4196
Talha <talhasahi86@gmail.com>
2020-03-09 18:32:04 +0500

    push

---
+++ Free Chat/Model/Massage.swift
@@ -0,0 +1,13 @@
+//
+//  Massage.swift
+//  Free Chat
+//
+//  Created by Talha on 07/03/2020.
+//  Copyright © 2020 Talha. All rights reserved.
+//
+
+import Foundation
+class Massage {
+    var sender : String = ""
+    var massageBody : String = ""
+}

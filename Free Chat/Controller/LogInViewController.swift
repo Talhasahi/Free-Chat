952c7fe4dd4adac0f47645302b5ce98cb18539c5
Talha <talhasahi86@gmail.com>
2020-03-09 18:31:50 +0500

    import Sv

--- Free Chat/Controller/LogInViewController.swift
+++ Free Chat/Controller/LogInViewController.swift
@@ -7,24 +7,23 @@
 //
 
 import UIKit
-
+import Firebase
+import SVProgressHUD
 class LogInViewController: UIViewController {
-
+    @IBOutlet weak var emailTextFeild: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logInPressed(_ sender: UIButton) {
+        Auth.auth().signIn(withEmail: emailTextFeild.text!, password: passwordTextField.text!) { (user, error) in
+             if error != nil {
+                print(error ?? "Error")
+                        }
+                        else {
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+                        }
+        }
     }
-    */
-
 }
